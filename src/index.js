d3210e2c0d174ac8ffdf08707377975f0965e9b4
rge_laptop <rge@plan.aau.dk>
2019-06-04 20:55:43 +0200

    express

--- src/index.js
+++ src/index.js
@@ -11,6 +11,22 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/app.html"));
 });
 
+// routes will go here
+app.get("/api/audio", function(req, res) {
+  var user_id = req.param("id");
+  var token = req.param("token");
+  var level = req.param("level");
+  console.log(user_id, token, Object.keys(level));
+});
+
+app.get("/api/position", function(req, res) {
+  var user_id = req.param("id");
+  var token = req.param("token");
+  var pos = req.param("pos");
+
+  console.log(user_id, token, pos);
+});
+
 reload(app)
   .then(() => {
     // reloadReturned is documented in the returns API in the README
--- src/public/audio_level.js
+++ src/public/audio_level.js
@@ -2,26 +2,23 @@ var rsm_block = [];
 var gps_block = [];
 
 function read_vars() {
-
-  let average = (array) => array.reduce((a, b) => a + b) / array.length;
-
-  console.log(average([rsm_block]).toString());
+  let average = array => array.reduce((a, b) => a + b) / array.length;
 
   console.log(gps_block);
-
+  window.location = `http://localhost:3000/api/audio?id=hej&token=DenErFin&level=${
+    average(rsm_block),
+  }`;
   rsm_block = [];
   gps_block = [];
-};
-
+}
 
 async function timer() {
+  console.log("started to read variables");
 
-  console.log('started to read variables');
-  
-  setInterval(() => { read_vars(); }, 5000);
-
-
-};
+  setInterval(() => {
+    read_vars();
+  }, 5000);
+}
 
 // get list af media devices
 
@@ -34,14 +31,14 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
 
 navigator.mediaDevices
   .enumerateDevices()
-  .then(function (devices) {
-    devices.forEach(function (device) {
+  .then(function(devices) {
+    devices.forEach(function(device) {
       console.log(
         device.kind + ": " + device.label + " id = " + device.deviceId
       );
     });
   })
-  .catch(function (err) {
+  .catch(function(err) {
     console.log(err.name + ": " + err.message);
   });
 
@@ -53,14 +50,14 @@ var constraints = { audio: true, video: false };
 
 navigator.mediaDevices
   .getUserMedia(constraints)
-  .then(function (mediaStream) {
+  .then(function(mediaStream) {
     var audioContext = new AudioContext();
     var mediaStreamSource = audioContext.createMediaStreamSource(mediaStream);
     var processor = audioContext.createScriptProcessor(2048, 1, 1);
     mediaStreamSource.connect(audioContext.destination);
     mediaStreamSource.connect(processor);
     processor.connect(audioContext.destination);
-    processor.onaudioprocess = function (e) {
+    processor.onaudioprocess = function(e) {
       var inputData = e.inputBuffer.getChannelData(0);
       var inputDataLength = inputData.length;
       var total = 0;
@@ -71,14 +68,14 @@ navigator.mediaDevices
       var rms = Math.sqrt(total / inputDataLength) * 100;
       document.getElementById("audio_level").innerHTML = rms.toString();
       // console.log(rms);
-      rsm_block.push(rms)
+      rsm_block.push(rms);
     };
 
     //   audio.onloadedmetadata = function(e) {
     //     audio.play();
     //   };
   })
-  .catch(function (err) {
+  .catch(function(err) {
     console.log(err.name + ": " + err.message);
   }); // always check for errors at the end.
 
@@ -93,7 +90,7 @@ function success(pos) {
   document.getElementById("accuracy").innerHTML = crd.accuracy.toString();
   document.getElementById("timestamp").innerHTML = pos.timestamp.toString();
 
-  gps_block.push(crd)
+  gps_block.push(crd);
 }
 
 function error(err) {
@@ -107,4 +104,4 @@ options = {
 };
 
 id = navigator.geolocation.watchPosition(success, error, options);
-timer()
+timer();
