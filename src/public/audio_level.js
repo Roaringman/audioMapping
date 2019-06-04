91439e2a58a757ae67e53272d716ea4d2e13c8d5
magnus-m <magnusmatias@yahoo.dk>
2019-06-04 20:11:05 +0200

    added timer

--- src/public/audio_level.js
+++ src/public/audio_level.js
@@ -1,3 +1,22 @@
+var rsm_block = [];
+var gps_block = [];
+
+function read_vars() {
+  // Do stuff like calculate mean values or so..
+  console.log(rsm_block, gps_block);
+};
+
+
+async function timer() {
+
+  console.log('started to read variables');
+
+  setTimeout(() => { read_vars(); }, 5000);
+
+  console.log(result);
+  // expected output: 'resolved'
+};
+
 // get list af media devices
 
 if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
@@ -9,14 +28,14 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
 
 navigator.mediaDevices
   .enumerateDevices()
-  .then(function(devices) {
-    devices.forEach(function(device) {
+  .then(function (devices) {
+    devices.forEach(function (device) {
       console.log(
         device.kind + ": " + device.label + " id = " + device.deviceId
       );
     });
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err.name + ": " + err.message);
   });
 
@@ -28,14 +47,14 @@ var constraints = { audio: true, video: false };
 
 navigator.mediaDevices
   .getUserMedia(constraints)
-  .then(function(mediaStream) {
+  .then(function (mediaStream) {
     var audioContext = new AudioContext();
     var mediaStreamSource = audioContext.createMediaStreamSource(mediaStream);
     var processor = audioContext.createScriptProcessor(2048, 1, 1);
     mediaStreamSource.connect(audioContext.destination);
     mediaStreamSource.connect(processor);
     processor.connect(audioContext.destination);
-    processor.onaudioprocess = function(e) {
+    processor.onaudioprocess = function (e) {
       var inputData = e.inputBuffer.getChannelData(0);
       var inputDataLength = inputData.length;
       var total = 0;
@@ -46,13 +65,14 @@ navigator.mediaDevices
       var rms = Math.sqrt(total / inputDataLength) * 100;
       document.getElementById("audio_level").innerHTML = rms.toString();
       console.log(rms);
+      rsm_block.push(rsm)
     };
 
     //   audio.onloadedmetadata = function(e) {
     //     audio.play();
     //   };
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err.name + ": " + err.message);
   }); // always check for errors at the end.
 
@@ -66,6 +86,8 @@ function success(pos) {
     crd.latitude.toString() + " ," + crd.longitude.toString();
   document.getElementById("accuracy").innerHTML = crd.accuracy.toString();
   document.getElementById("timestamp").innerHTML = pos.timestamp.toString();
+
+  gps_block.push(crd)
 }
 
 function error(err) {
